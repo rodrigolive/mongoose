0a97a63646296eab71af6bd7960e4674800b2e46
Arthur Wolf <wolf.arthur@gmail.com>
2010-08-27 14:29:26 +0200

    solved problem with moose, now stuck on problem with find_one

--- lib/Mongoose/Join/Relational.pm
+++ lib/Mongoose/Join/Relational.pm
@@ -3,10 +3,31 @@ use Moose;
 use Data::Dumper;
 extends 'Mongoose::Join';
 
+use Moose::Util::TypeConstraints;
+use Moose::Meta::TypeConstraint::Parameterizable;
+use Moose::Meta::TypeConstraint::Registry;
+
+my $REGISTRY = Moose::Meta::TypeConstraint::Registry->new;
+$REGISTRY->add_type_constraint(
+    Moose::Meta::TypeConstraint::Parameterizable->new(
+        name                 => 'Mongoose::Join::Relational',
+        package_defined_in   => __PACKAGE__,
+        parent               => find_type_constraint('Item'),
+        constraint           => sub { die 'constrained' },
+        constraint_generator => sub {
+            my $type_parameter = shift;
+
+            #print "constraint_generator...@_\n";
+            return sub { return {} };
+        }
+    )
+);
+
+Moose::Util::TypeConstraints::add_parameterizable_type(
+    $REGISTRY->get_type_constraint('Mongoose::Join::Relational') );
+
 has reciprocal => ( isa => 'Str', is => 'rw');
-has with_class => ( isa => 'Str', is => 'rw');
 has owner => ( isa => 'Any', is => 'rw');
-has buffer => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 
 sub add{
     my ( $self, @objs ) = @_;
@@ -20,6 +41,8 @@ sub add{
 sub find{
     my ( $self, $opts, @scope ) = @_;
     my $class = $self->with_class;
+    use Data::Dumper;
+    print Dumper { $self->reciprocal => $self->owner};
     return $class->find( { $self->reciprocal => $self->owner}, @scope ); #We find based on a reference in the Ball objects
 }
 
