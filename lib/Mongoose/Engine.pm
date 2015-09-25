b155741e16cd28f9b6490e0b78b32c393f6ec25c
diegok <diego@freekeylabs.com>
2015-09-25 02:10:35 +0200

    Warning note on README and docs on Engines

--- lib/Mongoose/Engine.pm
+++ lib/Mongoose/Engine.pm
@@ -3,4 +3,14 @@ use Moose::Role;
 
 with ( Mongoose->_mongodb_v1 ? 'Mongoose::Engine::V1' : 'Mongoose::Engine::V0' );
 
+=head1 NAME
+
+Mongoose::Engine - Mongoose Engine loader
+
+=head1 DESCRIPTION
+
+This is just a loader for the real engine L<Mongoose::Engine::V1> for the new MongoDB driver v1.x.x
+or L<Mongoose::Engine::V0> if you still use an old version of the driver.
+
+=cut
 1;
